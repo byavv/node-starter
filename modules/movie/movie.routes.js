1c8023b0f6cab7d89c5d347008e5f65879c0dc23
Aksenchyk Viacheslav <aksenchyk.v@gmail.com>
2017-02-16 21:58:43 +0300

    fix git ignore needed file

---
+++ modules/movie/movie.routes.js
@@ -0,0 +1,21 @@
+const controller = require('./movie.controller')
+    , express = require('express')
+    , middleware = require('../../middleware')
+    ;
+
+module.exports = function (app) {
+    const ctr = controller(app);
+    const router = express.Router();
+
+    router.get('/'
+        , middleware.mongoStatus(app)
+        , middleware.queryFilter(app), ctr.find);
+    router.post('/', ctr.create);
+    router.get('/findOne', middleware.queryFilter(app), ctr.findOne);
+    router.get('/:id', ctr.findById);
+    router.put('/:id', ctr.update);
+    router.delete('/:id', ctr.remove);
+
+    // ...
+    return router;
+}
